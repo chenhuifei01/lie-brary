bd47ac222679ab96606e4fe20bbd588d4d49b650
Reza R Pratama <rezarzky@users.noreply.github.com>
2023-03-05 22:48:53 -0600

    delete unecessary files

--- lie_brary/scripts/scrap/key.py
+++ lie_brary/scripts/scrap/key.py
@@ -1,21 +1,16 @@
-'''
-This is file to store the credential
-you can use your own credentials
-'''
-
 # Twitter Credentials
-api_key = ''
-api_secret_key = ''
-access_token = ''
-access_token_secret = ''
+api_key = '5ziWfVLWY4QygWCy3AzD2UQbQ'
+api_secret_key = 'bLdFyVDeDiiGzJaMYwlHwq5LAIbeS3J53Z1gK2ITCRTZVnbHMR'
+access_token = '115651874-xWpSpguXOlah1PtpcmXC7VjWAcryLxnGG1MxF141'
+access_token_secret = 'U3Ie5qhDa3f5DbFikPgor3xL7o3SHjhVwqdl2haSPYuv7'
 
 
 # Authenticate to Reddit
-CLIENT_ID = ''
-SECRET_KEY = ''
-pw = ''
-user = ''
+CLIENT_ID = 'shyhwdBJkuFVOeR-cI-piw'
+SECRET_KEY = 'A984oPGBir78gCC8Ce8RBe1X9K3chg'
+pw = 'An4021080'
+user = 'chenhuifei1'
 
 
 # OPENAI API
-OPENAI_KEY = ''
+OPENAI_KEY = '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'
